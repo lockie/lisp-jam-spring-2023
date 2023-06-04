33497dd5cdd2314daef91ee9f5347d7853661078
Andrew Kravchuk <awkravchuk@gmail.com>
2023-06-04 10:46:46 +0200

    Minor tweaks to win condition

--- src/win.lisp
+++ src/win.lisp
@@ -9,6 +9,6 @@
     (when (< (distance player-x player-y position-x position-y)
              (* 4f0 +scaled-tile-size+ +scaled-tile-size+))
       (al:show-native-message-box (cffi:null-pointer)
-                                  "You win!" "" "Thanks for playing!"
+                                  "You won!" "" "Thanks for playing!"
                                   (cffi:null-pointer) 0)
       (setf *quit* t))))

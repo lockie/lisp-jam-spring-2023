b6fde1e8e6478341b19d91e4b4b42c5e6714b63d
Andrew Kravchuk <awkravchuk@gmail.com>
2023-06-04 22:33:33 +0200

    Readme improvements

--- src/win.lisp
+++ src/win.lisp
@@ -9,6 +9,6 @@
     (when (< (distance player-x player-y position-x position-y)
              (* 4f0 +scaled-tile-size+ +scaled-tile-size+))
       (al:show-native-message-box (cffi:null-pointer)
-                                  "You won!" "" "Thanks for playing!"
+                                  "You won!" "" "You've beat the game!"
                                   (cffi:null-pointer) 0)
       (setf *quit* t))))
